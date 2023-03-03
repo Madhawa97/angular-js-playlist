bc9edaaefcf6189b9f3fc2aad1262f984eece3a5
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-03-03 17:39:18 +0530

    list a object items using ng-repeat

--- app/app.js
+++ app/app.js
@@ -3,7 +3,39 @@ var myAppHoldingVar = angular.module("myApp", []);  // pass dependencies in a ar
 // likewise the config and run method
 
 
-myAppHoldingVar.controller("formController", ['$scope', function($scope){ // a controller and $scope as MODEL to pass data between
-  $scope.message = "Greetings";
-  $scope.weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+myAppHoldingVar.controller("productController", ['$scope', function($scope){ // a controller and $scope as MODEL to pass data between
+  $scope.products = [
+    {
+      name: "MacBook Air 2020",
+      price: "1000 USD",
+    },
+    {
+      name: "MacBook Air 2022",
+      price: "1200 USD",
+    },
+    {
+      name: "MacBook Pro 13'",
+      price: "1400 USD",
+    },
+    {
+      name: "MacBook Pro 14'",
+      price: "1900 USD",
+    },
+    {
+      name: "MacBook Air 16'",
+      price: "2400 USD",
+    },
+    {
+      name: "Apple Watch",
+      price: "400 USD",
+    },
+    {
+      name: "iPad Air",
+      price: "800 USD",
+    },
+    {
+      name: "iPad Pro",
+      price: "1100 USD",
+    },
+  ];
 }]);
