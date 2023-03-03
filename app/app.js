93869ff74aaaf013dd3abeb6a03f65c17345230d
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-03-03 17:50:52 +0530

    Add a currency filter

--- app/app.js
+++ app/app.js
@@ -7,35 +7,35 @@ myAppHoldingVar.controller("productController", ['$scope', function($scope){ // 
   $scope.products = [
     {
       name: "MacBook Air 2020",
-      price: "1000 USD",
+      price: 1000,
     },
     {
       name: "MacBook Air 2022",
-      price: "1200 USD",
+      price: 1200,
     },
     {
       name: "MacBook Pro 13'",
-      price: "1400 USD",
+      price: 1400,
     },
     {
       name: "MacBook Pro 14'",
-      price: "1900 USD",
+      price: 1900,
     },
     {
       name: "MacBook Air 16'",
-      price: "2400 USD",
+      price: 2400,
     },
     {
       name: "Apple Watch",
-      price: "400 USD",
+      price: 400,
     },
     {
       name: "iPad Air",
-      price: "800 USD",
+      price: 800,
     },
     {
       name: "iPad Pro",
-      price: "1100 USD",
+      price: 1100,
     },
   ];
 }]);
