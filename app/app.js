401cd3ef2cb48d00815e528bae7898322775dd3a
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-03-03 14:19:15 +0530

    Add a module

---
+++ app/app.js
@@ -0,0 +1,17 @@
+var myAppHoldingVar = angular.module("myApp", []);  // pass dependencies in a array
+// module is like a container for all components, such as controllers.
+// likewise the config and run method
+
+myApp.config(function(){    // this runs just before the app runs. prep code , eg: routing
+
+})
+
+
+myApp.run(function(){ // runs as the app runs
+
+})
+
+
+myApp.controller(function(){ // a controller
+
+})
