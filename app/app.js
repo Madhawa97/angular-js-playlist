4d920ed8c2c25baa71877300d36b039701cb4fd3
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-03-03 14:47:10 +0530

    Add a controller

--- app/app.js
+++ app/app.js
@@ -2,16 +2,8 @@ var myAppHoldingVar = angular.module("myApp", []);  // pass dependencies in a ar
 // module is like a container for all components, such as controllers.
 // likewise the config and run method
 
-myApp.config(function(){    // this runs just before the app runs. prep code , eg: routing
 
-})
-
-
-myApp.run(function(){ // runs as the app runs
-
-})
-
-
-myApp.controller(function(){ // a controller
-
-})
+myAppHoldingVar.controller("formController",function($scope){ // a controller and $scope as MODEL to pass data between
+  $scope.message = "Greetings";
+  $scope.weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+});
