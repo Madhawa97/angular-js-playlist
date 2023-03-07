2adeb095807885d6cdf70ef2583bc29f9d017819
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-03-07 18:02:27 +0530

    Add some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./*.{html,js}"
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#1e293b',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
     extend: {},
   },
   plugins: [],
